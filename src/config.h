598fc16872c22f1c0c0bf6c1c2e7833e4352cade
Shakti Rajput <shakti@availfinance.in>
2023-07-28 17:13:07 +0200

    Modified minor changes and Printing Changes

--- src/config.h
+++ src/config.h
@@ -3,7 +3,7 @@
 #include <string>
 using namespace std;
 
-const int MAX_SIZE = 4;
+const int MAX_SIZE = 100;
 const string outputPath = "Result/";
 const string DATA = "Data/watdiv10M.txt";
 const string QUERY = "Data/Query.txt";
