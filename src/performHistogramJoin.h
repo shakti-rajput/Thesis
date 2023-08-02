c86e2b52eaf8b47a45c7140654ae5f3941437bcb
Shakti Rajput <shakti@availfinance.in>
2023-08-02 10:51:49 +0200

    HashStore changed

--- src/performHistogramJoin.h
+++ src/performHistogramJoin.h
@@ -5,10 +5,13 @@
 
 using namespace std;
 
-void semiJoinOpConVertices(unordered_map<long long int, list<Sobit>>& sobitTables,
-                           const Graph& g,
-                           unordered_map<long long int, string>& decodeStringToData,
-                           unordered_map<long long int, unordered_map<long long int, list<Sobit>>>& hashStore);
+void semiJoinOpConVertices(unordered_map<long long int, list<Sobit>> &sobitTables,
+                           const Graph &g,
+                           unordered_map<long long int, string> &decodeStringToData,
+                           unordered_map<long long int,
+                                         unordered_map<long long int,
+                                                       unordered_map<long long int,
+                                                                     unordered_map<long long int, list<Sobit>>>>> &hashStore);
 
 void semiJoinOpNonConVertices(unordered_map<long long int, list<Sobit>> &sobitTables,
                               const Graph &g, unordered_map<long long int, string> decodeStringToData);
